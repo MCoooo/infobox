9277a6eb6ab290f0f75ca5efb1b705ab56b9ac66
Martin Coomber <martin.coomber@hotmail.com>
2020-08-30 18:39:09 +0100

    feedList function working for session data. application data still not

--- assets/js/inforunner_applications.js
+++ assets/js/inforunner_applications.js
@@ -7,9 +7,51 @@ let appData = JSON.parse(appRawData);
 
 
 const layerList = document.getElementById('layerList');
-console.log("layer list");
+console.log("layer list", layerList);
 
+//This function will recurse through array of arrays.
+//If the level is an object is will recurse targetting
+//The properties under it. The obj name is the json node
+//Being looked for so is used on first run only
+function feedList (obj, list,objName="null"){
+    if (objName == "null"){
+        for(var key in obj){
+            if(obj[key] instanceof Object){
+                console.log("obj key is ", key);
+                feedList(obj[key],list);
+            } else {
+                console.log("not obj key is ", key);
+                const li = document.createElement('li');
+                const itemText = document.createTextNode(key+": " + obj[key]);
+                li.appendChild(itemText);
+                list.appendChild(li);
+            }
+        }
+    } else {
+        for(var key in obj){
+            if(key == objName){
+                if(obj[key] instanceof Object){
+                    console.log("obj key is ", key);
+                    feedList(obj[key],list);
+                } else {
+                    const li = document.createElement('li');
+                    console.log("not obj key is ", key);
+                    if (parseInt(key)){
+                        const itemText = document.createTextNode(obj[key]);
+                    } else {
+                        const itemText = document.createTextNode(key+": " + obj[key]);
+                    }
+                    li.appendChild(itemText);
+                    list.appendChild(li);
+                }
+            }
+        }
+    }
+}
 
+feedList(appData, layerList,"LayerData");
+
+/*
 for(var attributename in appData){
     console.log(attributename+": "+ appData[attributename]);
     if(attributename == 'LayerList') {
@@ -35,5 +77,5 @@ for(var attributename in appData){
 
 
 }
-
+*/
 
--- assets/js/inforunner_session.js
+++ assets/js/inforunner_session.js
@@ -5,78 +5,44 @@ const sessionfs = require('fs');
 let sessionRawData = sessionfs.readFileSync('inforunner.json');
 let sessionData = JSON.parse(sessionRawData);
 
-console.log(sessionData);
 const ulist = document.getElementById('ulist') //document.querySelector("ul.user");
 const mlist = document.getElementById('mlist');
 const runnerlist = document.getElementById('runnerlist');
 
-function feedList (obj, objName, list){
-for(var key in obj){
-    if(key == objName){
-        if(obj[key] instanceof Object){
-            feedList(obj[key], objName);
-        } else {
-            console.log("found " +key + " : " + obj[key]);
-            const li = document.createElement('li');
-            const itemText = document.createTextNode(key+": " + obj[key]);
-            li.appendChild(itemText);
-            list.appendChild(li);
-        }
-    }
-}
-}
 
-feedList(sessionData, "UserData", ulist);
-    /*
-        if((attributename=="Hostname") || (attributename=="IPAddress") || (attributename=="LayerCount")){
-            mlist.appendChild(li);
-        }
-        if((attributename=="RunnerVersion")||(attributename=="TimeStamp")|| (attributename=="Duration")){
-            runnerlist.appendChild(li);
-        }
-    } 
-    */
-
-/*
-const layerList = document.getElementById('layerList');
-
-console.log(ulist);
-
-
-for(var attributename in data){
-    if(attributename != 'LayerList'){
-        const li = document.createElement('li');
-        const itemText = document.createTextNode(attributename+": "+data[attributename]);
-        li.appendChild(itemText);
-        if((attributename=="Username") || (attributename=="LogOnTime")){
-            ulist.appendChild(li);
-        } else {
-            mlist.appendChild(li);
+//This function will recurse through array of arrays.
+//If the level is an object is will recurse targetting
+//The properties under it. The obj name is the json node
+//Being looked for so is used on first run only
+function feedList (obj, list,objName="null"){
+    if (objName == "null"){
+        for(var key in obj){
+            if(obj[key] instanceof Object){
+                feedList(obj[key],list);
+            } else {
+                const li = document.createElement('li');
+                const itemText = document.createTextNode(key+": " + obj[key]);
+                li.appendChild(itemText);
+                list.appendChild(li);
+            }
         }
     } else {
-        const layers = data[attributename];
-        layers.forEach(async function(layer) {
-            const packages = layer.Packages;
-            
-            const ul = document.createElement('ul');
-            ul.innerText = layer.Name;
-            packages.forEach(element => {
-                const li = document.createElement('li');
-                const itemText = document.createTextNode(element);
-                li.appendChild(itemText);
-                ul.appendChild(li);
-            });
-            layerList.appendChild(ul);
-
-            //const itemText = document.createTextNode(layer.Name);
-            
-            //layerList.appendChild(li);
-          })
-       
-        console.log(attributename+": "+data[attributename]);
+        for(var key in obj){
+            if(key == objName){
+                if(obj[key] instanceof Object){
+                    feedList(obj[key],list);
+                } else {
+                    const li = document.createElement('li');
+                    const itemText = document.createTextNode(key+": " + obj[key]);
+                    li.appendChild(itemText);
+                    list.appendChild(li);
+                }
+            }
+        }
     }
-
-
 }
-*/
 
+feedList(sessionData, ulist,"UserData");
+feedList(sessionData, mlist,"MachineData");
+feedList(sessionData, runnerlist,"RunnerData");
+ 
